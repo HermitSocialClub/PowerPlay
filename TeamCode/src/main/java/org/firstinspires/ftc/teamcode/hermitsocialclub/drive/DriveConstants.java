4bafb7cb0a1b173d703dc5e658a3b1ef03a00715
hallea <halleaqp@gmail.com>
2022-12-05 20:11:01 -0800

    roadrunner stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hermitsocialclub/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hermitsocialclub/drive/DriveConstants.java
@@ -35,8 +35,8 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
-            getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV));
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0.2, 0, 0.01,
+            getMotorVelocityF(280 / 60 * TICKS_PER_REV));  //MAX_RPM
 
     /*
      * These are physical constants that can be determined from your robot (including the track
@@ -48,7 +48,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 2; // in
     public static double GEAR_RATIO = 0.5; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 12.5; // in
+    public static double TRACK_WIDTH = 12.61; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -56,9 +56,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
-    public static double kA = 0;
-    public static double kStatic = 0;
+    public static double kV = 0.0235; //1.0 / rpmToVelocity(MAX_RPM);
+    public static double kA = 0.00276;
+    public static double kStatic = 0.015;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -88,10 +88,10 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
 
      */
-    public static double MAX_VEL = 30.264009229581674;//16;
-    public static double MAX_ACCEL = 30.264009229581674;//60.52801845916335;
-    public static double MAX_ANG_VEL = Math.toRadians(138.72);
-    public static double MAX_ANG_ACCEL = Math.toRadians(138.72);
+    public static double MAX_VEL = 22; //30.264009229581674;//16;
+    public static double MAX_ACCEL = 30;//30.264009229581674;//60.52801845916335;
+    public static double MAX_ANG_VEL = Math.toRadians(208.66648554252808);//(138.72);
+    public static double MAX_ANG_ACCEL = Math.toRadians(180);//(138.72);
 
     public static DcMotorSimple.Direction[] DIRECTIONS = new DcMotorSimple.Direction[]{DcMotorSimple.Direction.FORWARD, DcMotorSimple.Direction.FORWARD,
             DcMotorSimple.Direction.REVERSE, DcMotorSimple.Direction.REVERSE};
